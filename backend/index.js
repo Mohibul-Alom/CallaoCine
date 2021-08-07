384626a703946c1ba43cca879d8fe09c77b7f1b9
Mohibul <tuhin28012@gmail.com>
2021-08-07 17:08:59 +0200

    FEAT: express working!!

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,18 @@
-console.log('Hola mundo');
+const express = require('express');
+
+const PORT = 3000;
+
+const app = express();
+const router = express.Router();
+
+app.use('/',router);
+
+
+router.get('/',(req,res) => {
+    res.send("Hola")
+});
+
+
+app.listen(PORT,() => {
+    console.log(`Server runnig in http://localhost:${PORT}`);
+});
---
+++ backend/readme.md
@@ -0,0 +1,2 @@
+1. Install node js
+2. In command line type: npm install
