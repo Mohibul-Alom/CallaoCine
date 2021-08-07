8245bc5b5624ae70277d0d446e3c28d222493005
Mohibul <tuhin28012@gmail.com>
2021-08-08 00:46:18 +0200

    FEAT: added movie model

---
+++ backend/models/Movies.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const {Schema} = mongoose;
+
+const movieSchema = new Schema(
+    {
+        title:{ type: String, required: true},
+        director: {type: String, required: true},
+        description:{ type: String, required: true},
+        duration:{ type: Number, required: true},
+        genere:[String],
+    },
+    { timestamps: true }
+)
+
+const Movie = mongoose.model('Movies',movieSchema);
+
+module.exports = Movie;
