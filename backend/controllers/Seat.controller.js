26bfaad3110a4a498046b9bc9f5a6a94be97dcd9
Mohibul <tuhin28012@gmail.com>
2021-08-08 23:01:09 +0200

    FEAT: added seat put route

--- backend/controllers/Seat.controller.js
+++ backend/controllers/Seat.controller.js
@@ -1,54 +1,65 @@
-const Seat = require('../models/Seat.model');
+const Seat = require("../models/Seat.model");
 
 const seatGet = async (req, res, next) => {
-
-    try {
-        
-        const seats = await Seat.find();
-        if(seats.length > 0) {
-            return res.status(200).json(seats);
-        }else {
-            const error = new Error("La collection seats está vacía");
-            throw error;
-        }
-
-    } catch (error) {
-        next(error);
+  try {
+    const seats = await Seat.find();
+    if (seats.length > 0) {
+      return res.status(200).json(seats);
+    } else {
+      const error = new Error("La collection seats está vacía");
+      throw error;
     }
-
-}
+  } catch (error) {
+    next(error);
+  }
+};
 
 const seatPost = async (req, res, next) => {
+  try {
+    const { row, number, price, booked } = req.body;
 
-    try {
-        const { row, number, price, booked } = req.body;
-    
-        const newTicket = new Seat({
-            row, 
-            number, 
-            price, 
-            booked: booked ==='false'? false:true,
-        });
+    const newSeat = new Seat({
+      row,
+      number,
+      price,
+      booked: booked === "false" ? false : true,
+    });
 
-        const createdTicket = await newTicket.save();
-        return res.status(200).json(createdTicket);
-
-      } catch (error) {
-        next(error);
-      }
-}
+    const createdTicket = await newSeat.save();
+    return res.status(200).json(createdTicket);
+  } catch (error) {
+    next(error);
+  }
+};
 
 const seatPut = async (req, res, next) => {
+  try {
+    const { id, row, number, price, booked } = req.body;
 
-}
+    const update = {};
 
-const seatDelete = async (req, res, next) => {
+    if(row) update.row = row;
+    if(number) update.number = number;
+    if(price) update.price = price;
+    if(booked) update.booked = booked;
 
-}
+    const updateSeat = await Seat.findByIdAndUpdate(
+        id,
+        update,
+        { new: true } // Usando esta opción, conseguiremos el documento actualizado cuando se complete el update
+    );
+    return res.status(200).json(updateSeat);
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+const seatDelete = async (req, res, next) => {};
 
 module.exports = {
-    seatGet,
-    seatPost,
-    seatPut,
-    seatDelete
-}
+  seatGet,
+  seatPost,
+  seatPut,
+  seatDelete,
+};
