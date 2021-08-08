6c75111c7e273fcef7d63d27bc3daa5fce7a68b0
Mohibul <tuhin28012@gmail.com>
2021-08-08 14:01:31 +0200

    FEAT: ticket model added

---
+++ backend/models/Ticket.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const {Schema} = mongoose;
+
+const ticketSchema = new Schema (
+    {
+        hasPaid = {type: Boolean,required: true},
+        num:{type:Number,required: true},
+        timeLeft: {type: Date,required: true},
+        day: {type:Date,required: true},
+        auditorium : {type:mongoose.Types.ObjectId,ref:'Movie'},
+        
+    },
+    {timestamps: true}
+);
+
+const Ticket = mongoose.model('Tickets',ticketSchema);
+
+module.exports = Ticket;
