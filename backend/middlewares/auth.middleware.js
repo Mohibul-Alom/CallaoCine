f4505a4c21241a69c71e7f63b366a26c8a9f426c
Mohibul <tuhin28012@gmail.com>
2021-08-09 17:24:15 +0200

    FEAT: auth middleware added

---
+++ backend/middlewares/auth.middleware.js
@@ -0,0 +1,16 @@
+const isAuth = (req,res,next) => {
+    return req.isAuthenticated() 
+    ? next()
+    : res.status(401).json("No estas autorizado");
+}
+
+const isAdmin = (req,res,next) => {
+    if(req.isAuthenticated()){
+        if(req.user.role == "admin"){
+            return next();
+        }
+        return res.status(403).json("No tienes permisos");
+    }else {
+        return res.status(401).json("No estas autorizado");
+    }
+}
