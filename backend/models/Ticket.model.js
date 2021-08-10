3432fd6234240404381c8d8572884052f1a2875e
Mohibul <tuhin28012@gmail.com>
2021-08-10 17:50:02 +0200

    DELETED: removed num of tickets

--- backend/models/Ticket.model.js
+++ backend/models/Ticket.model.js
@@ -5,7 +5,6 @@ const {Schema} = mongoose;
 const ticketSchema = new Schema (
     {
         hasPaid: {type:Boolean,required: true},
-        num:{type:Number,required: true},
         timeLeft: {type: Number,required: true},
         day: {type: Date,required: true},
         auditorium : {type:mongoose.Types.ObjectId,ref:'Auditoriums'},
