5f3b927adf1f2859166fed3912eb07be10eab99d
Mohibul <tuhin28012@gmail.com>
2021-08-10 17:48:23 +0200

    FEAT: added user get and user ticket update

---
+++ backend/controllers/User.controller.js
@@ -0,0 +1,56 @@
+const User = require('../models/User.model');
+
+const userGet = async (req, res, next) => {
+
+    try {
+
+        // if(req.user){
+        //     console.log('Existe usuario con id-->',req.user.id)
+        // }
+
+        const user = await User.findById(req.user.id);
+
+        if(user !== null) {
+            return res.status(200).json(user);
+        }else{
+            throw new Error("[Error] No se ha encontrado al usuario")
+        }
+
+    } catch (error) {
+        next(error);
+    }
+
+}
+
+const userUpdateTickets = async (req, res, next) => {
+
+    try{
+
+        const {userId,ticketId} = req.body;
+
+        const update = {};
+
+        if(ticketId) update.ticketId = ticketId;
+
+        const updateUser = await User.findByIdAndUpdate(
+            userId,
+            update,
+            {new:true}
+        )
+
+        return res.status(200).json(updateUser);
+        
+    }catch (error) {
+        return next(error);
+    }
+}
+
+const userDeleteTickets = async (req, res, next) => {
+
+}
+
+module.exports = {
+    userGet,
+    userUpdateTickets,
+    userDeleteTickets,
+}
