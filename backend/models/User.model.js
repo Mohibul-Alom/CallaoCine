deb143337bc0a79ce5fbb750dc5687aba45acee7
Mohibul <tuhin28012@gmail.com>
2021-08-11 22:10:00 +0200

    FEAT: added ticket ref in user model

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -9,7 +9,7 @@ const userSchema = new Schema (
         password:{type: String, required: true},
         role: { type: String, required: true, enum: ['user', 'admin'], default: 'user' },
         name:{type: String, required: true},
-        tickets:{type:mongoose.Types.ObjectId,ref:'Tickets',default:null},
+        tickets:{type:mongoose.Types.ObjectId,ref:'Tickets'},
     },
     {timestamps: true}
 );
